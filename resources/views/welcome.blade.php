4ac4fb9073cb939bfebf1d2d6e608f6b02c049af
kumar ravi <hi@kumarravi.me>
2020-06-07 02:32:59 +0530

    home page initial design complete
    Signed-off-by: kumar ravi <hi@kumarravi.me>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <div class="container mx-auto px-6 py-4">
             <div class="flex items-center justify-between">
                 <div>
-                    <a class="flex items-center text-gray-800 hover:text-indigo-600" href="#">
+                    <a class="flex items-center text-gray-800 hover:text-indigo-600" href="{{url('/')}}">
                         <svg class="h-6 w-6 sm:h-8 sm:w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
                         </svg>
@@ -148,48 +148,52 @@
 
     <section class="bg-gray-800 pattern py-20">
         <div class="max-w-5xl px-6 mx-auto text-center">
-            <h2 class="text-2xl font-semibold text-white">Projects</h2>
+            <h2 class="text-2xl font-semibold text-white">Open Source Projects</h2>
 
             <div class="flex items-center justify-center mt-10">
                 <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
                     <div class="max-w-xs w-full">
-                        <div class="flex items-center justify-center h-56 bg-white border-b-8 border-teal-400 rounded-md overflow-hidden">
-                            <img class="object-cover h-8" src="https://premium-tailwindcomponents.netlify.app/assets/svg/tailwindcomponent-dark.svg" alt="">
+                        <div class="flex items-center justify-center  bg-white border-b-8 border-teal-400 rounded-md overflow-hidden">
+                            <img class="object-cover " src="{{url('/multisheet.png')}}" alt="">
                         </div>
 
-                        <a href="#" class="block bg-gray-700 mt-5 rounded-md overflow-hidden transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+                        <a href="https://github.com/kumarravisingh/laravel-import-multisheet-excel-with-formula" target="_blank" class="block bg-gray-700 mt-5 rounded-md overflow-hidden transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
                             <div class="py-2 px-3 text-center text-sm">
-                                <p class="text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                <p class="text-gray-300">
+                                    Demo to import excel file with multiple
+                                    sheet and formula using Laravel.
+                                </p>
 
-                                <span class="block text-gray-500 mt-2">tailwindcomponents.com</span>
+
+                                <span class="block text-gray-500 mt-2">Laravel Multisheet</span>
                             </div>
                         </a>
                     </div>
 
                     <div class="max-w-xs w-full">
-                        <div class="flex items-center justify-center h-56 bg-white border-b-8 border-teal-400 rounded-md overflow-hidden">
-                            <img class="object-cover h-8" src="https://premium-tailwindcomponents.netlify.app/assets/svg/tailwindcomponent-dark.svg" alt="">
+                        <div class="flex items-center justify-center bg-white border-b-8 border-teal-400 rounded-md overflow-hidden">
+                            <img class="object-cover" src="{{url('/lara-count.png')}}" alt="">
                         </div>
 
-                        <a href="#" class="block bg-gray-700 mt-5 rounded-md overflow-hidden transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+                        <a href="https://github.com/kumarravisingh/laraconcountdown" target="_blank" class="block bg-gray-700 mt-5 rounded-md overflow-hidden transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
                             <div class="py-2 px-3 text-center text-sm">
-                                <p class="text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                <p class="text-gray-300">Countdown application for laravel annual event</p>
 
-                                <span class="block text-gray-500 mt-2">tailwindcomponents.com</span>
+                                <span class="block text-gray-500 mt-2">Laracon Countdown</span>
                             </div>
                         </a>
                     </div>
 
                     <div class="max-w-xs w-full">
-                        <div class="flex items-center justify-center h-56 bg-white border-b-8 border-teal-400 rounded-md overflow-hidden">
-                            <img class="object-cover h-8" src="https://premium-tailwindcomponents.netlify.app/assets/svg/tailwindcomponent-dark.svg" alt="">
+                        <div class="flex items-center justify-center bg-white border-b-8 border-teal-400 rounded-md overflow-hidden">
+                            <img class="object-cover" src="{{url('/docker-legacy.png')}}" alt="">
                         </div>
 
-                        <a href="#" class="block bg-gray-700 mt-5 rounded-md overflow-hidden transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+                        <a href="https://github.com/kumarravisingh/docker-legacy" target="_blank" class="block bg-gray-700 mt-5 rounded-md overflow-hidden transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
                             <div class="py-2 px-3 text-center text-sm">
-                                <p class="text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                <p class="text-gray-300">Demo application to run different version of php using docker</p>
 
-                                <span class="block text-gray-500 mt-2">tailwindcomponents.com</span>
+                                <span class="block text-gray-500 mt-2">Docker Legacy</span>
                             </div>
                         </a>
                     </div>
@@ -197,7 +201,7 @@
             </div>
 
             <div class="flex items-center justify-center mt-12">
-                <a class="flex items-center text-white hover:underline hover:text-gray-200" href="#">
+                <a class="flex items-center text-white hover:underline hover:text-gray-200" target="_blank" href="https://github.com/kumarravisingh">
                     <span>View More On Github</span>
 
                     <svg class="h-5 w-5 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
