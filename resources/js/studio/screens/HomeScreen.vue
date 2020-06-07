2a7274a80cdc29fbcce85a64464b9a21a2b7a296
kumar ravi <hi@kumarravi.me>
2020-06-07 09:21:27 +0530

    updated post liting page title
    Signed-off-by: kumar ravi <hi@kumarravi.me>

--- resources/js/studio/screens/HomeScreen.vue
+++ resources/js/studio/screens/HomeScreen.vue
@@ -19,7 +19,7 @@
         <div class="mt-5 pt-5">
             <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1 col-md-12 mt-3">
                 <h1 class="font-serif">Kumar Ravi</h1>
-                <p class="lead text-secondary">A Laravel Publishing Platform</p>
+                <p class="lead text-secondary">A Laravel Developer</p>
 
                 <main role="main" class="mt-5">
                     <div v-if="posts.length > 0">
